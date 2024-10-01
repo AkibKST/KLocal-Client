bffd9239f9c7262d340e0bb09f3c1eed81a0341f
AkibKST <akibkst22@gmail.com>
2024-10-01 15:00:01 +0600

    Dynamic Page Title By Route Using React Helmet

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -13,16 +13,19 @@ const PopularMenu = () => {
       });
   }, []);
   return (
-    <section className="mb-12">
+    <section className="mb-12 space-y-6 text-center">
       <SectionTitle
         heading="From Our Menu"
         subHeading="Popular Menu"
       ></SectionTitle>
-      <div className="grid md:grid-cols-2 gap-10">
+      <div className="grid md:grid-cols-2 gap-10 text-left px-6">
         {menu.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
+      <button className="btn btn-outline border-0 border-b-4 mt-4">
+        View Full Menu
+      </button>
     </section>
   );
 };
---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>KLocal | Menu</title>
+      </Helmet>
+      <h2>Menu page</h2>
+    </div>
+  );
+};
+
+export default Menu;
